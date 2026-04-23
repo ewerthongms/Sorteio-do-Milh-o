829f14f2e8f76485bda5159e2bd0d57ae75aba02
ewerthongms <ewerthongms@gmail.com>
2026-04-23 19:09:02 -0300

    90%

--- Script2.js
+++ Script2.js
@@ -7,8 +7,21 @@ function generateNumber(){
     const max = Math.floor(document.getElementById("input2").value)
     const exit = document.getElementById("resultadoo")
 
+
+   
+
     const result = Math.floor(Math.random() * (max - min + 1)) + min;
     
-    exit.value = result    
+    exit.value = result  
+
+     if(min >= max){ // O INPUT NUNCA PODERA SER MAIOR QUE O MAXIMO.
+        exit.value = alert("O valor mínimo deve ser menor que o valor máximo.");
+        exit.classList.add("error");
+    }else {const result = Math.floor(Math.random() * (max - min + 1)) + min;
+    
+    exit.value = result 
+
+    } 
      
 }
+
